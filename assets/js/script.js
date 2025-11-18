d51954afd6cf72655fa03438148626c15f7c0cb1
vitoFirman <149038991+vitoFirman@users.noreply.github.com>
2025-11-19 06:00:23 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar-custom');
+  const btnTop = document.getElementById('btnTop');
+
+  if (window.scrollY > 20) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+
+  if (window.scrollY > 100) {
+    btnTop.classList.add('show');
+  } else {
+    btnTop.classList.remove('show');
+  }
+});
+
+document.getElementById('btnTop').addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+
+// loader hilang 1 detik
+window.addEventListener('load', () => {
+  spinnerWrapperEl.classList.add('hidden');
+  setTimeout(() => {
+    spinnerWrapperEl.style.display = 'none';
+  }, 1000); // match duration CSS
+});
+
+// loader muncul saat pindah halaman
+window.addEventListener('beforeunload', () => {
+  spinnerWrapperEl.style.display = 'flex';
+  setTimeout(() => {
+    spinnerWrapperEl.classList.remove('hidden');
+  }, 10);
+});
